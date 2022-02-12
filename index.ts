e3d0ade52f00d0703fe5a2bf28b7cb485b40d382
3bodka <3bodka.102@gmail.com>
2022-02-13 01:57:39 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+//######################################################################
+//###########################           SSTech       ###################
+//###########################   Programming Courses  ###################
+//######################################################################
+console.log('hey abdullah');
+console.log('fuck you');
+let abdullah: number;
+let ebubekir: number;
+let gurhan: number;
+gurhan = 1000;
+abdullah = -1573;
+ebubekir = 638;
+console.log(abdullah + ebubekir + gurhan);
+
+let numb1: number;
+let numb2: number;
+let numb3: number;
+numb1 = 523;
+numb2 = 673;
+numb3 = numb1 + numb2;
+console.log(numb3);
+
+let num1, num2, num3, num4: number;
+num1 = 243;
+num2 = 536;
+num3 = 737;
+num4 = num1 + num2 + num3;
+console.log(num4);
